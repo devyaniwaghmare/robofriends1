881f280babd3fe9bc2f47f049c81f39523b5cbfc
devyani <devyani271196@gmail.com>
2019-10-13 11:13:09 +0530

    Updates

---
+++ precache-manifest.55ff0474b492527c61e8d87d1ec98e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c3ba13183a4799cd8f572d6b4272ae",
+    "url": "/devyaniwaghmare/robofriends1/index.html"
+  },
+  {
+    "revision": "ae864eaa3b76e795cf5d",
+    "url": "/devyaniwaghmare/robofriends1/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "578bce01c6dab8c91069",
+    "url": "/devyaniwaghmare/robofriends1/static/css/main.54b28ce3.chunk.css"
+  },
+  {
+    "revision": "ae864eaa3b76e795cf5d",
+    "url": "/devyaniwaghmare/robofriends1/static/js/2.2e980e75.chunk.js"
+  },
+  {
+    "revision": "578bce01c6dab8c91069",
+    "url": "/devyaniwaghmare/robofriends1/static/js/main.01abb013.chunk.js"
+  },
+  {
+    "revision": "1c4a7d2afd3778a6c087",
+    "url": "/devyaniwaghmare/robofriends1/static/js/runtime~main.dd732b7f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/devyaniwaghmare/robofriends1/static/media/SEGA.12a3db8b.woff"
+  }
+]);
